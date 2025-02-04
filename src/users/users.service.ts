14a136a0cf186c5b1b3e5e2748a9280e5adda628
andrea.mangolini <andrea.mangolini@spindox.it>
2025-02-04 12:54:27 +0100

    feat: add auth module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,23 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 import { SafeUserDto } from './dto/safe-user';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly databaseService: DatabaseService) { }
+  constructor(private readonly prisma: DatabaseService) { }
 
-  create(createUserDto: Prisma.UserCreateInput): Promise<SafeUserDto> {
-    return this.databaseService.user.create({
-      data: createUserDto,
+  async create(createUserDto: Prisma.UserCreateInput): Promise<SafeUserDto> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = { ...createUserDto, password: hashedPassword };
+    return this.prisma.user.create({
+      data: newUser,
     });
   }
 
   findAll(): Promise<SafeUserDto[]> {
-    return this.databaseService.user.findMany({ omit: { password: true } });
+    return this.prisma.user.findMany({ omit: { password: true } });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUnique({ where: { id: id } });
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email: email } });
   }
 
   update(id: number, updateUserDto: SafeUserDto) {
